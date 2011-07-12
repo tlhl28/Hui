d7ba3bc41dae42fee1168e5318cc1e1b2c71c1c8
Harold Luo <tlhl28@gmail.com>
2011-07-12 17:53:39 +0800

    deleted:    app/controllers/.application_controller.rb.swp 	modified:   app/controllers/application_controller.rb 	modified:   app/controllers/home_controller.rb

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,53 @@
 class HomeController < ApplicationController
-	def index
-		@waves = User.find(
-	end
+  def timeline
+
+  end
+
+  def login
+    if request.get?
+      session[:jump] = params[:jump]
+    elsif request.post?
+      user = User.login(params[:name], params[:password])
+      if user
+        jump = session[:jump]
+        clean_session_cookie
+        #user.set_session(session, request, @site)
+        #user.remember_me(cookies, request) if params[:persist]
+        if jump
+          redirect_to(jump)
+        else
+          error_stickie t(:message_3, :scope => [:txt, :controller, :blog])
+        end
+      end
+    end  
+  end
+
+  def forgot_password
+    return unless request.post?
+    user = User.find_by_email(params[:email])
+    if user
+      user.forgot_password(request)
+      notice_stickie t(:reset_password_email_send, :scope => [:txt, :controller, :blog])
+    else
+      error_stickie t(:no_user_found, :scope => [:txt, :controller, :blog])
+    end
+  end
+
+  def reset_password
+    return unless request.post?
+    user = User.find_by_reset_password_key(params[:key])
+    if user && user.reset_password_key_expires_at > Time.now
+      if params[:new] == params[:_new]
+        user.password = params[:new]
+        user.reset_password_key = nil
+        user.save
+        notice_stickie t(:password_reseted, :scope => [:txt, :controller, :blog])
+      else
+        error_stickie t(:confirm_dismatch, :scope => [:txt, :controller, :blog])
+      end    
+    else
+      error_stickie t(:reset_password_expired, :scope => [:txt, :controller, :blog])
+    end 
+  end
+
 end 
